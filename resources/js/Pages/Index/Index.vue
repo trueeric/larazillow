630c17dfa27ce81f4eaa876684a19ea074146a97
Eric Uen <ikpassat@gmail.com>
2023-08-01 11:39:01 +0800

    030  Layouts

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>Vue Index Page</div>
-    <Link href="/hello">Show Page</Link>
-    <div>The message is {{ message }}</div>
+    <main-layout>
+        <div>Vue Index Page</div>
+        <Link href="/hello">Show Page</Link>
+        <div>The message is {{ message }}</div>
+    </main-layout>
 </template>
 <script setup>
 import { Link } from "@inertiajs/vue3";
+import MainLayout from "../../Layouts/MainLayout.vue";
 defineProps({
     message: String,
 });
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>Vue Show Page</div>
-    <Link href="/">Index </Link>
+    <main-layout>
+        <div>Vue Show Page</div>
+        <Link href="/">Index </Link>
+    </main-layout>
 </template>
+
 <script setup>
 import { Link } from "@inertiajs/vue3";
+import MainLayout from "../../Layouts/MainLayout.vue";
 </script>
