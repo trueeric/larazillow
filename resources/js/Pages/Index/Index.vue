52c0ed2d268bf5f72360887bfc70c2788a1065b1
Eric Uen <ikpassat@gmail.com>
2023-08-04 18:21:10 +0800

    032 Persistent layouts with Inertia

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,14 +1,19 @@
 <template>
-    <main-layout>
-        <div>Vue Index Page</div>
-        <Link href="/hello">Show Page</Link>
-        <div>The message is {{ message }}</div>
-    </main-layout>
+    <div>Vue Index Page</div>
+    <Link href="/hello">Show Page</Link>
+    <div>The message is {{ message }}</div>
 </template>
 <script setup>
 import { Link } from "@inertiajs/vue3";
-import MainLayout from "../../Layouts/MainLayout.vue";
 defineProps({
     message: String,
 });
+</script>
+
+<script>
+import MainLayout from "../../Layouts/MainLayout.vue";
+
+export default {
+    layout: MainLayout,
+};
 </script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,11 +1,16 @@
 <template>
-    <main-layout>
-        <div>Vue Show Page</div>
-        <Link href="/">Index </Link>
-    </main-layout>
+    <div>Vue Show Page</div>
+    <Link href="/">Index </Link>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
+</script>
+
+<script>
 import MainLayout from "../../Layouts/MainLayout.vue";
+
+export default {
+    layout: MainLayout,
+};
 </script>
