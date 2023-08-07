f6ad620a7769820491dae2cc8b8f2b6c4c30b9e5
Eric Uen <ikpassat@gmail.com>
2023-08-07 15:10:39 +0800

    050 Computed Data and Conditional Rendering

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,13 +2,34 @@
     <Link href="/">Main Page </Link>&nbsp
     <Link href="/hello">Show Page</Link>
     <!-- <div>The page with time {{ timer }}</div> -->
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
+    <div>{{ y }}</div>
     <slot>Default</slot>
 </template>
 
 <script setup>
-import { Link } from "@inertiajs/vue3";
+import { computed } from "vue";
+import { Link, usePage } from "@inertiajs/vue3";
+
+// USE page.props.flash.success!!   DO NOT page.props.value.flash.success
+const page = usePage();
+// console.log("kk1:", page);
+// console.log('kk:',page.props.value.flash.success);
+
+const flashSuccess = computed(() => page.props.flash.success);
+// console.log("kk2:", flashSuccess);
+
 // import { ref } from "vue";
 
 // const timer = ref(0);
 // setInterval(() => timer.value++, 1000);
 </script>
+
+<style scoped>
+.success {
+    background-color: green;
+    color: white;
+}
+</style>
