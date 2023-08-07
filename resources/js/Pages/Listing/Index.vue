d70f07919af8de00c035a723fec4c07efb945803
Eric Uen <ikpassat@gmail.com>
2023-08-07 21:53:36 +0800

    054  Laravel Route Names in Vue

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,15 +1,22 @@
 <template>
     <div v-for="listing in listings" :key="listing.id">
         <span>
-            <Link :href="`/listing/${listing.id}`">
+            <!-- <Link :href="`/listing/${listing.id}`"> -->
+            <Link :href="route('listing.show', listing.id)">
                 <listing-address :listing="listing" /> </Link
             >&nbsp;
         </span>
         <span>
-            <Link :href="`/listing/${listing.id}/edit`">Edit</Link>&nbsp;
+            <!-- <Link :href="`/listing/${listing.id}/edit`">Edit</Link>&nbsp; -->
+            <Link :href="route('listing.edit', listing.id)">Edit</Link>&nbsp;
         </span>
         <span>
-            <Link :href="`/listing/${listing.id}`" method="DELETE" as="button"
+            <!-- <Link :href="`/listing/${listing.id}`" method="DELETE" as="button" >Delete</Link
+            >-->
+            <Link
+                :href="route('listing.destroy', listing.id)"
+                method="DELETE"
+                as="button"
                 >Delete</Link
             >
         </span>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import MainLayout from '@/Layouts/MainLayout.vue'
-
+import { ZiggyVue } from 'ziggy'
 createInertiaApp({
   resolve: async name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
@@ -15,6 +15,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(ZiggyVue)
       .mount(el)
   },
 })
