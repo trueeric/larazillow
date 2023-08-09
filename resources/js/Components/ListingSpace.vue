724b93601f09465dad63268c3d74f190d490cfc3
Eric Uen <ikpassat@gmail.com>
2023-08-09 10:36:22 +0800

    060 Listing Details

---
+++ resources/js/Components/ListingSpace.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <span class="font-bold">{{ listing.beds }} bds</span>
+        <span class="text-gray-400"> | </span>;
+        <span class="font-bold">{{ listing.baths }} ba</span>
+        <span class="text-gray-400"> | </span>
+        <span class="font-bold">{{ listing.area }} </span> m²
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    listing: Object,
+});
+</script>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,31 +1,36 @@
 <template>
-    <div v-for="listing in listings" :key="listing.id">
-        <span>
-            <!-- <Link :href="`/listing/${listing.id}`"> -->
-            <Link :href="route('listing.show', listing.id)">
-                <listing-address :listing="listing" /> </Link
-            >&nbsp;
-        </span>
-        <span>
-            <!-- <Link :href="`/listing/${listing.id}/edit`">Edit</Link>&nbsp; -->
-            <Link :href="route('listing.edit', listing.id)">Edit</Link>&nbsp;
-        </span>
-        <span>
-            <!-- <Link :href="`/listing/${listing.id}`" method="DELETE" as="button" >Delete</Link
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <Box v-for="listing in listings" :key="listing.id">
+            <div>
+                <!-- <Link :href="`/listing/${listing.id}`"> -->
+                <Link :href="route('listing.show', listing.id)">
+                    <listing-space :listing="listing" class="text-lg" />
+                    <listing-address :listing="listing" class="text-gray-500" />
+                </Link>
+            </div>
+            <div>
+                <!-- <Link :href="`/listing/${listing.id}/edit`">Edit</Link>&nbsp; -->
+                <Link :href="route('listing.edit', listing.id)">Edit</Link>
+            </div>
+            <div>
+                <!-- <Link :href="`/listing/${listing.id}`" method="DELETE" as="button" >Delete</Link
             >-->
-            <Link
-                :href="route('listing.destroy', listing.id)"
-                method="DELETE"
-                as="button"
-                >Delete</Link
-            >
-        </span>
+                <Link
+                    :href="route('listing.destroy', listing.id)"
+                    method="DELETE"
+                    as="button"
+                    >Delete</Link
+                >
+            </div>
+        </Box>
     </div>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
 import ListingAddress from "@/Components/ListingAddress.vue";
+import ListingSpace from "@/Components/ListingSpace.vue";
+import Box from "@/Components/UI/Box.vue";
 
 defineProps({
     listings: Array,
