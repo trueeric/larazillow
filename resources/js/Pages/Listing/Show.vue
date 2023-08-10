45c4799feb346e9c4533351c0d81386d3d104cb4
Eric Uen <ikpassat@gmail.com>
2023-08-10 10:02:36 +0800

    062 Listing Page + Vue Named Slots

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,9 +1,31 @@
 <template>
-    <listing-address :listing="listing" />
+    <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <Box class="md:col-span-7 flex items-center w-full">
+            <div class="w-full text-center font-medium text-gray-500">
+                No images
+            </div>
+        </Box>
+        <!-- 為了要在小螢幕單欄時，圖改成顯在資料欄的下方，資料欄包在一起 -->
+        <div class="md:col-span-5 flex flex-col gap-4">
+            <Box>
+                <template #header>Basic info </template>
+                <Price :price="listing.price" class="text-2xl font-bold" />
+                <listing-space :listing="listing" class="text-lg" />
+                <listing-address :listing="listing" class="text-gray-500" />
+            </Box>
+            <Box>
+                <template #header>Offer </template>
+                Make an offer
+            </Box>
+        </div>
+    </div>
 </template>
 
 <script setup>
 import ListingAddress from "@/Components/ListingAddress.vue";
+import ListingSpace from "@/Components/ListingSpace.vue";
+import Price from "@/Components/Price.vue";
+import Box from "@/Components/UI/Box.vue";
 
 defineProps({
     listing: Object,
