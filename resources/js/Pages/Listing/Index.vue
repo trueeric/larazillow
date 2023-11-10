bcdf8a4b5291750bb8e93336fb41919d41804d7b
Eric Uen <ikpassat@gmail.com>
2023-11-10 10:25:59 +0800

    105_Filtering the Data

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,4 +1,6 @@
 <template>
+    <Filters :filters="filters" />
+
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         <Listing
             v-for="listing in listings.data"
@@ -8,7 +10,7 @@
     </div>
     <div
         v-if="listings.data.length"
-        class="w-full flex justify-center mt-8 mb-8"
+        class="w-full flex justify-center mt-4 mb-4"
     >
         <Pagination :links="listings.links" />
     </div>
@@ -16,8 +18,10 @@
 <script setup>
 import Listing from "@/Pages/Listing/Index/Components/Listing.vue";
 import Pagination from "@/Components/UI/Pagination.vue";
+import Filters from "@/Pages/Listing/Index/Components/Filters.vue";
 
 defineProps({
     listings: Object,
+    filters: Object,
 });
 </script>
