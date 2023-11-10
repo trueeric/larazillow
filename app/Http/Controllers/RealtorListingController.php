52301f99c8e02af928a03cdcea4b8a17953f9eb8
Eric Uen <ikpassat@gmail.com>
2023-11-10 17:37:54 +0800

    111_Showing the Realtor Listings

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 class RealtorListingController extends Controller
 {
     public function index()
     {
-        return inertia('Realtor/Index');
+
+        return inertia('Realtor/Index',
+            ['listings' => Auth::user()->listings]
+        );
     }
 }
--- resources/js/Pages/Realtor/Index.vue
+++ resources/js/Pages/Realtor/Index.vue
@@ -1,3 +1,39 @@
 <template>
-    <div>Hello !!</div>
+    <H1 class="text-3xl mb-4">Your Listings</H1>
+    <section class="mb-8">Filters</section>
+    <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
+        <box v-for="listing in listings" :key="listings.id">
+            <div
+                class="flex flex-col md:flex-row gap-2 md:items-center justify-between"
+            >
+                <div>
+                    <div class="xl:flex items-center gap-2">
+                        <Price
+                            :price="listing.price"
+                            class="text-2xl font-medium"
+                        />
+                        <ListingSpace :listing="listing" />
+                    </div>
+                    <ListingAddress :listing="listing" class="to-gray-500" />
+                </div>
+                <div
+                    class="flex items-center gap-1 text-gray-600 dark:text-gray-300"
+                >
+                    <Link class="btn-outline text-xs font-medium">Preview</Link>
+                    <Link class="btn-outline text-xs font-medium">Edit</Link>
+                    <Link class="btn-outline text-xs font-medium">Delete</Link>
+                </div>
+            </div>
+        </box>
+    </section>
 </template>
+
+<script setup>
+import Box from "@/components/UI/Box.vue";
+import Price from "@/components/Price.vue";
+import ListingSpace from "@/components/ListingSpace.vue";
+import ListingAddress from "@/components/ListingAddress.vue";
+import { Link } from "@inertiajs/vue3";
+
+defineProps({ listings: Array });
+</script>
