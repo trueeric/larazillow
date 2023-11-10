d68699c222c05630a4a402bcdbd4639cda0eb3ba
Eric Uen <ikpassat@gmail.com>
2023-11-10 12:18:01 +0800

    108_Filtering Using Local Query Scope

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -24,37 +24,23 @@ class ListingController extends Controller
     public function index(Request $request)
     {
         $filters = $request->only(['priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo']);
-        // dd($filters['priceFrom']);
 
         //  not use when
         // if ($filters['priceFrom'] ?? false) {
         //     $query->where('price', '>=', $filters['priceFrom']);
         // }
 
+        // scopeActive
+        // $query->where()->active;
+
         return inertia(
             'Listing/Index',
             [
                 'filters'  => $filters,
-                'listings' => Listing::orderByDesc('created_at')
-                    ->when(
-                        $filters['priceFrom'] ?? false,
-                        fn($query, $value) => $query->where('price', '>=', $value)
-                    )->when(
-                    $filters['priceTo'] ?? false,
-                    fn($query, $value) => $query->where('price', '<=', $value)
-                )->when(
-                    $filters['beds'] ?? false,
-                    fn($query, $value) => $query->where('beds', (int) $value < 6 ? "=" : '>=', $value)
-                )->when(
-                    $filters['baths'] ?? false,
-                    fn($query, $value) => $query->where('baths', (int) $value < 6 ? "=" : '>=', $value)
-                )->when(
-                    $filters['areaFrom'] ?? false,
-                    fn($query, $value) => $query->where('area', '>=', $value)
-                )->when(
-                    $filters['areaFrom'] ?? false,
-                    fn($query, $value) => $query->where('area', '<=', $value)
-                )->paginate(10)->withQueryString(),
+                'listings' => Listing::mostRecent()
+                    ->filter($filters)
+                    ->paginate(10)
+                    ->withQueryString(),
             ]
         );
     }
