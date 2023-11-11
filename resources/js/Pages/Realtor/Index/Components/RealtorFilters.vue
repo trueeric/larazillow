e6ade08bab13937f3231d5b73794024bd90c10ee
Eric Uen <ikpassat@gmail.com>
2023-11-11 08:39:06 +0800

    116_The Debounce Function

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -17,16 +17,22 @@
 <script setup>
 import { reactive, watch } from "vue";
 import { router } from "@inertiajs/vue3";
+import { debounce } from "lodash";
 
 const filterForm = reactive({
     deleted: false,
 });
 
 // watch()接受的類型僅有 reactive / ref / computed
-watch(filterForm, () =>
-    router.get(route("realtor.listing.index"), filterForm, {
-        preserveState: true,
-        preserveScroll: true,
-    })
+watch(
+    filterForm,
+    debounce(
+        () =>
+            router.get(route("realtor.listing.index"), filterForm, {
+                preserveState: true,
+                preserveScroll: true,
+            }),
+        1000
+    )
 );
 </script>
