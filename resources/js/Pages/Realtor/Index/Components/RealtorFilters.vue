c7524e45cb95501591a3fb75f9ebdaef7c203b85
Eric Uen <ikpassat@gmail.com>
2023-11-12 06:05:12 +0800

    119_Customizable Sorting - Front End

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -1,6 +1,6 @@
 <template>
     <form>
-        <div class="mb-4 mt-0 flex flex-wrap gap-2">
+        <div class="mb-4 mt-0 flex flex-wrap gap-4">
             <div class="flex flex-nowrap items-center gap-2">
                 <input
                     id="deleted"
@@ -10,17 +10,59 @@
                 />
                 <label for="deleted">Deleted</label>
             </div>
+            <div>
+                <select class="input-filter-l w-26" v-model="filterForm.by">
+                    <option value="created_at">Added</option>
+                    <option value="price">Price</option>
+                </select>
+                <select class="input-filter-r w-32" v-model="filterForm.order">
+                    <option
+                        v-for="option in sortOptions"
+                        :key="option.value"
+                        :value="option.value"
+                    >
+                        {{ option.label }}
+                    </option>
+                </select>
+            </div>
         </div>
     </form>
 </template>
 
 <script setup>
-import { reactive, watch } from "vue";
+import { reactive, watch, computed } from "vue";
 import { router } from "@inertiajs/vue3";
 import { debounce } from "lodash";
 
+const sortLabels = {
+    created_at: [
+        {
+            label: "Latest",
+            value: "desc",
+        },
+        {
+            label: "Oldest",
+            value: "asc",
+        },
+    ],
+    price: [
+        {
+            label: "Pricey",
+            value: "desc",
+        },
+        {
+            label: "Cheapest",
+            value: "asc",
+        },
+    ],
+};
+
+const sortOptions = computed(() => sortLabels[filterForm.by]);
+
 const filterForm = reactive({
     deleted: false,
+    by: "created_at",
+    order: "desc",
 });
 
 // watch()接受的類型僅有 reactive / ref / computed
