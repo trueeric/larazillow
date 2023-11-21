4685ec8d3d571da1f21edfe2d803d8f3fbaef831
Eric Uen <ikpassat@gmail.com>
2023-11-22 05:20:38 +0800

    154_Sending Notification When Offer is Made

--- app/Http/Controllers/ListingOfferController.php
+++ app/Http/Controllers/ListingOfferController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use App\Models\Offer;
+use App\Notifications\OfferMade;
 use Illuminate\Http\Request;
 
 class ListingOfferController extends Controller
@@ -13,12 +14,16 @@ class ListingOfferController extends Controller
         // 延用ListingPolicy中的view規則
         $this->authorize('view', $listing);
 
-        $listing->offers()->save(
+        $offer = $listing->offers()->save(
             Offer::make(
                 $request->validate([
                     'amount' => 'required|integer|min:1|max:20000000',
                 ])
             )->bidder()->associate($request->user())
+        );
+        // 僅通知listing擁有者
+        $listing->owner->notify(
+            new OfferMade($offer)
         );
 
         return redirect()->back()->with(
--- app/Notifications/OfferMade.php
+++ app/Notifications/OfferMade.php
@@ -2,8 +2,8 @@
 
 namespace App\Notifications;
 
+use App\Models\Offer;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
@@ -14,8 +14,9 @@ class OfferMade extends Notification
     /**
      * Create a new notification instance.
      */
-    public function __construct()
-    {
+    public function __construct(
+        private Offer $offer
+    ) {
         //
     }
 
@@ -26,7 +27,7 @@ class OfferMade extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['mail'];
+        return ['database'];
     }
 
     /**
@@ -35,9 +36,9 @@ class OfferMade extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
     }
 
     /**
@@ -48,7 +49,10 @@ class OfferMade extends Notification
     public function toArray(object $notifiable): array
     {
         return [
-            //
+            'offer_id'   => $this->offer->id,
+            'listing_id' => $this->offer->listing_id,
+            'amount'     => $this->offer->amount,
+            'bidder_id'  => $this->offer->bidder_id,
         ];
     }
 }
